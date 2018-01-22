78b40bcbd9cf26bdd905c2a798650e6089288a4e
jerryhibernet <jisjfidj@123.com>
2018-01-22 21:38:26 +0800

    this is e3-manager

---
+++ e3-manager/e3-manager-service/src/main/java/cn/e3mall/service/impl/TbItemServiceImpl.java
@@ -0,0 +1,21 @@
+package cn.e3mall.service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import cn.e3mall.mapper.TbItemMapper;
+import cn.e3mall.pojo.TbItem;
+import cn.e3mall.service.TbItemService;
+
+@Service
+public class TbItemServiceImpl implements TbItemService{
+	
+	@Autowired
+	private TbItemMapper tbItemMapper;
+
+	@Override
+	public TbItem findById(Long id) {		
+		return tbItemMapper.selectByPrimaryKey(id);
+	}
+
+}
